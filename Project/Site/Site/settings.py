6b67592db8d22a6cf7d46c83172f7a9d5848cea5
DimitryBond <126660963+DimitryBond@users.noreply.github.com>
2024-01-18 21:37:28 +0500

    move static4

--- Project/Site/Site/settings.py
+++ Project/Site/Site/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
@@ -115,8 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = '/var/www/static'
+STATIC_URL = '/static/'
 #STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
