981ead47ef143af3a73d92eac07bd1237ee47f53
DimitryBond <126660963+DimitryBond@users.noreply.github.com>
2024-01-23 21:11:07 +0500

    fix update

--- Project/Site/main/static/main/js/themes.js
+++ Project/Site/main/static/main/js/themes.js
@@ -3,12 +3,8 @@ let lightThemeButton = document.querySelector('.theme-button-light');
 
 darkThemeButton.onclick = function () {
   document.body.classList.remove('light');
-  darkThemeButton.classList.add('active');
-  lightThemeButton.classList.remove('active');
 };
 
 lightThemeButton.onclick = function () {
   document.body.classList.add('light');
-  lightThemeButton.classList.add('active');
-  darkThemeButton.classList.remove('active');
 };
